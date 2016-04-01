b60f68a5f7957c57d687cf79b3a607529396ba01
mitsuishihidemi <hidemi.mitsuishi@gmail.com>
2016-04-01 00:20:32 -0300

    refactor css

--- client/src/components/container.js
+++ client/src/components/container.js
@@ -1,12 +1,16 @@
 import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 
 export default class Container extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="container">
-        <div className="sidebar">
-          <h1>sc - home care</h1>
-        </div>
+        <div className="sidebar"></div>
         <div className="content"></div>
       </div>
     );
